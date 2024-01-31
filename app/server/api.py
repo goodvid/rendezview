c1268815c9721f2cb49df2a4baa245831226a423
goodvid <vuppalav@purdue.edu>
2024-01-30 23:09:40 -0500

    Add database functionality

--- app/server/api.py
+++ app/server/api.py
@@ -1,9 +1,15 @@
 import time
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/server/models.py
@@ -0,0 +1,13 @@
+from . import db 
+
+class User(db.Model):
+    pass
+
+class Event(db.Model):
+    pass
+
+class Blog(db.Model):
+    pass
+
+class PrivateEvent(db.Model):
+    pass
