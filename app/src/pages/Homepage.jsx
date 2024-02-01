e280015a7f17e6ba1efe7ad1732b36f0d5d3fc36
visathongdee <tthongde@purdue.edu>
2024-01-31 19:09:13 -0500

    Set up react-router-dom and added MUI dependency

---
+++ app/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import logo from "../logo.svg";
+import "../App.css";
+
+function Homepage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Homepage</p>
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
