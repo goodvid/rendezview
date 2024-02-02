a6681bc1bf822302fe7ca139c134ed627ad7c177
goodvid <vuppalav@purdue.edu>
2024-02-02 17:23:14 -0500

    Initial commit to create database

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import {
-  BrowserRouter as BrowserRouter,
+  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+import Login from "./pages/login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
