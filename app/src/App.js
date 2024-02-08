48393143c84b8e51f1848a58d26452faac35d467
goodvid <vuppalav@purdue.edu>
2024-02-08 17:00:47 -0500

    add username

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,15 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from "./views/Login/Login";
+import React from 'react';
+import './App.css';
+import Main from './views/Main';
+import Username from './views/Username/Username'
+import Register from './views/Register/Register';
 
-import Login from "./pages/login";
-import React from "react";
-import Main from "./views/Main";
-// import Login from './views/Login/Login';
-import Register from "./views/Register/Register";
-import Quiz from "./pages/Quiz";
-import Homepage from "./pages/Homepage";
-import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -18,8 +19,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/username" element={<Username />} />
         </Routes>
       </BrowserRouter>
     </div>
