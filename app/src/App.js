737d2ef27c18c7ca0af2b750ac67fd5868f8e095
visathongdee <tthongde@purdue.edu>
2024-02-08 17:10:16 -0500

    Changed file structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,12 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./views/Login/Login";
-import React from 'react';
-import './App.css';
-import Main from './views/Main';
-import Username from './views/Username/Username'
-import Register from './views/Register/Register';
-
+import React from "react";
+import "./App.css";
+import Main from "./views/Main";
+import Username from "./views/Username/Username";
+import Register from "./views/Register/Register";
+import Profile from "./views/Profile/Profile";
+import Quiz from "./views/Quiz/Quiz";
 
 function App() {
   return (
@@ -20,6 +17,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/username" element={<Username />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
--- app/src/views/Username/Username.js
+++ app/src/views/Username/Username.js
@@ -1,43 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 
 function Login() {
-    const [inputData, setInputData] = useState({
-      // Initial state, adjust based on your form fields
-      profile: ""
-      
+  const [inputData, setInputData] = useState({
+    // Initial state, adjust based on your form fields
+    profile: "",
+  });
+
+  const handleChange = (event) => {
+    // Update the inputData state when form fields change
+    setInputData({
+      ...inputData,
+      [event.target.name]: event.target.value,
     });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-     const handleChange = (event) => {
-       // Update the inputData state when form fields change
-       setInputData({
-         ...inputData,
-         [event.target.name]: event.target.value,
-       });
-     };
-     const handleSubmit = (event) => {
-       event.preventDefault();
-
-       // Send data to Flask server
-       fetch("http://localhost:5000/submit-data", {
-         method: "POST",
-         headers: {
-           "Content-Type": "application/json",
-         },
-         body: JSON.stringify(inputData),
-       })
-         .then((response) => response.json())
-         .then((data) => {
-           console.log("Success:", data);
-           // Handle success
-         })
-         .catch((error) => {
-           console.error("Error:", error);
-           // Handle errors
-         });
-     };
+    // Send data to Flask server
+    fetch("http://localhost:5000/submit-data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        // Handle success
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle errors
+      });
+  };
   return (
     <div className="w-full h-full">
       <Navbar />
@@ -48,16 +48,13 @@ function Login() {
             Profile Name
           </span>
           <input className="w-[360px] h-[45px] border-login-blue outline rounded-md pl-2" />
-          
-          
+
           <button
             onChange="/events"
             className="w-[360px] h-[45px] bg-login-blue text-white font-bold rounded-lg mt-[30px]"
           >
             Set profile name
           </button>
-         
-       
         </div>
       </div>
     </div>
