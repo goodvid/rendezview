44c1890f7566642572fde88c30edb70ce8ba0526
ts549 <sun955@purdue.edu>
2024-02-13 16:49:56 -0500

    a

--- app/src/views/Register/Register.js
+++ app/src/views/Register/Register.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Register.css'
 import Navbar from '../../components/Navbar/Navbar';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Register() {
+
+    const [email, setEmail] = useState("");
+
+    const updateEmail = (event) => {
+        if (event != null) {
+            setEmail(event.target.value);
+        }
+    };
+
+    const [password, setPassword] = useState("");
+
+    const updatePassword = (event) => {
+        if (event != null) {
+            setPassword(event.target.value);
+        }
+    };
+
+    const register = () => {
+        axios.post("http://localhost:3000/user/register", {
+            email: email,
+            password: password
+        })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+        setEmail("");
+        setPassword("");
+    }
+
     return (
         <div className='w-full h-full'>
             <Navbar />
@@ -13,9 +46,9 @@ function Register() {
                     <div className='flex flex-row justify-center items-center gap-[220px]'>
                         <div className='flex flex-col justify-center items-center gap-1'>
                             <span className='w-[360px] text-gray-500 text-left mt-[40px]'>Email</span>
-                            <input className='w-[360px] h-[45px] border-login-blue outline rounded-md pl-2' />
+                            <input onChange={updateEmail} className='w-[360px] h-[45px] border-login-blue outline rounded-md pl-2' />
                             <span className='w-[360px] text-gray-500 text-left mt-[20px]'>Password</span>
-                            <input className='w-[360px] h-[45px] border-login-blue outline rounded-md pl-2' />
+                            <input onChange={updatePassword} className='w-[360px] h-[45px] border-login-blue outline rounded-md pl-2' />
                         </div>
                         <div className='mt-8 w-[300px]'>
                             <span>Choose Profile Picture</span>
@@ -30,9 +63,12 @@ function Register() {
                         <span className='text-gray-500'>OR</span>
                         <span className='text-gray-500 font-bold'>Manually Add Preferences</span>
                     </div>
-                    <button
-                    href='/login'
-                    className='w-[260px] h-[45px] bg-login-blue text-white font-medium rounded-lg mt-[80px]'>Create Account</button>
+                    <Link
+                    to='/login'>
+                        <button
+                        className='w-[260px] h-[45px] bg-login-blue text-white font-medium rounded-lg mt-[80px]'
+                        onClick={register}>Create Account</button>
+                    </Link>
                     <span className='font-normal text-gray-500 mt-4'>Already have an account?</span>
                     <Link
                     id="log-in"
