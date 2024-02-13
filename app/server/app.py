30cd95c41c5f5bb6d69f9d181b0aee4d50d15e2b
ts549 <sun955@purdue.edu>
2024-02-13 16:49:35 -0500

    commit

--- app/server/app.py
+++ app/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models import db, User, Event, Blog, PrivateEvent  # Importing the db instance and models
 
 app = Flask(__name__)
@@ -10,6 +10,47 @@ db.init_app(app)  # Initialize db with your Flask app
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/user/register', methods=["POST"])
+def register():
+    data = request.json
+
+    email = data['email']
+    password = data['password']
+
+    print(email)
+    print(password)
+
+    id = 1
+
+    while (db.query("select * from users where userID=" + str(id)) != None):
+        id += 1
+
+    db.query("insert into users values (" + str(id)
+                + "," + email
+                + "," + email
+                + "," + password
+                + "," + "None"
+                + "," + "None"
+                + "," + "None"
+                + "," + "None"
+                + "," + "None"
+                + "," + "None"
+                + "," + "None"
+                + "," + "None" + ")")
+
+    return {'200': 'success'}
+
+@app.route('/user/login', methods=["POST"])
+def login():
+    data = request.json
+
+    email = data['email']
+    password = data['password']
+
+    res = db.execute('select * from USERS where email=' + email + ' AND password=' + password + ';')
+
+    return {'200': email + " " + password}
 
 if __name__ == '__main__':
     app.run(debug=True)
