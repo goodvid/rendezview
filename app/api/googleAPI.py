4fb1fa911da0a7be0f7ab3706675fe8b2916256c
Suj <Stimils02@purdue.edu>
2024-02-18 15:05:16 -0500

    Google API flow

--- app/api/googleAPI.py
+++ app/api/googleAPI.py
@@ -1,12 +1,16 @@
 import datetime
 import json
+import traceback
 import requests
+import time
+import errno
 import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from pprint import pprint
 
 
 class GoogleAPI:
@@ -15,6 +19,7 @@ class GoogleAPI:
     SCOPES = ["https://www.googleapis.com/auth/calendar",
               'https://www.googleapis.com/auth/userinfo.email', 'openid']
     PORT_NO = 8080
+    TIME_BREAK = 30
 
     def __init__(self) -> None:
         pass
@@ -26,6 +31,7 @@ class GoogleAPI:
         if os.path.exists("token.json"):
             creds = Credentials.from_authorized_user_file(
                 "token.json", self.SCOPES)
+
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
@@ -34,7 +40,16 @@ class GoogleAPI:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     "credentials.json", self.SCOPES
                 )
+            try:
                 creds = flow.run_local_server(port=self.PORT_NO)
+            except OSError as e:
+                if e.errno == errno.EADDRINUSE:
+                    print(f"Port {self.PORT_NO} in use, retrying...")
+                    time.sleep(self.TIME_BREAK)
+                    creds = flow.run_local_server(port=self.PORT_NO)
+                else:
+                    raise
+
             # Save the credentials for the next run
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
@@ -43,10 +58,8 @@ class GoogleAPI:
 
     def get_user_email(self, credentials):
         """Fetches the user's email using their credentials.
-
         Args:
             credentials: OAuth2 credentials obtained during the authorization process.
-
         Returns:
             str: The user's email address.
         """
@@ -57,10 +70,8 @@ class GoogleAPI:
     def read_token_from_file(self, token_file_path='token.json'):
         """
         Reads and returns the token information from a specified token file.
-
         Args:
             token_file_path (str): Path to the token file.
-
         Returns:
             dict: Token information, including access token and refresh token.
         """
@@ -75,37 +86,29 @@ class GoogleAPI:
             print(f"Error decoding JSON from {token_file_path}.")
             return None
 
-    def revoke_google_token(self, token: str):
+    def revoke_google_token(self, file_path='token.json'):
         """
         Revokes the given token, effectively detaching the Google account from the application.
-
         Args:
             token (str): The token to be revoked. This could be an access token or a refresh token.
         """
-        revoke_endpoint = 'https://oauth2.googleapis.com/revoke'
-        params = {'token': token}
-        headers = {'content-type': 'application/x-www-form-urlencoded'}
-
-        response = requests.post(
-            revoke_endpoint, params=params, headers=headers)
-
-        if response.status_code == 200:
-            print("Token successfully revoked.")
-        else:
-            print(
-                f"Failed to revoke token. Status code: {response.status_code}, Response: {response.text}")
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        # token_info = {}
+        # with open(file_path, 'w') as token_file:
+        #     json.dump(token_info, token_file, indent=2)
 
     def save_credentials_and_email(self, creds, email, file_path='token.json'):
-        """Saves credentials and the user's email to a file.
-
+        """
+        Saves credentials and the user's email to a file.
         Args:
             creds: The OAuth2 credentials to save.
             email: The user's email address to save.
             file_path (str): Path to the file where the credentials and email should be saved.
         """
         token_info = json.loads(
-            creds.to_json())  # Convert credentials to a dict
-        token_info['account'] = email  # Add the email to the dict
+            creds.to_json())
+        token_info['account'] = email
         with open(file_path, 'w') as token_file:
             json.dump(token_info, token_file, indent=2)
 
@@ -113,43 +116,44 @@ class GoogleAPI:
 
         return
 
+    def test(self):
+        """
+        Test function:
+        """
+        while True:
+            try:
+                x = input("Enter a letter: ")
+                if x == "a":
+                    print("-------Authenticating...-------------")
+                    creds = self.authenticate()
+                    user_email = self.get_user_email(creds)
+                    self.save_credentials_and_email(creds, user_email)
+                    pprint(creds)
+                    print(user_email)
+
+                elif x == "r":
+                    print("-------Reading Data....-------------")
+                    token_data = self.read_token_from_file()
+                    pprint(token_data)
+
+                elif x == "d":
+                    print(
+                        "-------Revoking or disconnecting from google account...-------------")
+                    self.revoke_google_token()
+                else:
+                    break
+            except Exception as e:
+                print(e)
+                print(traceback.format_exc())
+                print("error occurred during test operation")
+
+        return
+
 
 if __name__ == "__main__":
-    pass
-    # # if os.path.exists("token.json"):
-    # #     os.remove("token.json")
-    # creds = None
-    # # The file token.json stores the user's access and refresh tokens, and is
-    # # created automatically when the authorization flow completes for the first time.
-    # if os.path.exists("token.json"):
-    #     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             "credentials.json", SCOPES
-    #         )
-    #         creds = flow.run_local_server(port=PORT_NO)
-    #     # Save the credentials for the next run
-    #     with open("token.json", "w") as token:
-    #         token.write(creds.to_json())
-
-    # try:
-    #     # user_email = get_user_email(creds)
-    #     # print(f"User's email: {user_email}")
-    #     # save_credentials_and_email(creds, user_email)
-    #     print(read_token_from_file())
-    # except:
-    #     print("Some error occurred")
-
-    # if os.path.exists("token.json"):
-    #     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # user_email = get_user_email(creds)
-    # print(f"User's email: {user_email}")
-    # Example usage
-    # token_data = read_token_from_file()
-    # if token_data:
-    #     print("Token data:", token_data)
-    # main()
+    google = GoogleAPI()
+    google.test()
+    # TODO
+    # Modify Redirect URI
+    # Change to dynamic PORT PORT_NO = 0 will handle that
+    # Save to DataBase
