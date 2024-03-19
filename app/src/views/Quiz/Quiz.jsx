2214bd9db23fc6babd69e0e0771010bf1e2e06a8
Akshaya Kumar <kumar455@purdue.edu>
2024-03-19 18:14:13 -0400

    real categories for quiz

--- app/src/views/Quiz/Quiz.jsx
+++ app/src/views/Quiz/Quiz.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import "../../styles.css";
 import { withAuth } from "../withAuth";
+import categories from "../../views/eventCategories.json";
 
 function Quiz() {
   let resp = false;
@@ -12,16 +13,6 @@ function Quiz() {
   const [selectedStr, setSelectedStr] = useState("");
   const [minSelected, setMinSelected] = useState(false);
   const [selected, setSelected] = useState([]);
-  const [tags, setTags] = useState([
-    //TODO: fix categories
-    "Comedy",
-    "Food",
-    "Film",
-    "Travel",
-    "Rock",
-    "Yoga",
-    "DIY",
-  ]);
   const [alertOpen, setAlertOpen] = useState(false);
 
   const handleSelect = useCallback(
@@ -90,16 +81,17 @@ function Quiz() {
           flexWrap="wrap"
           width="100%"
         >
-          {tags.map((name) => (
+          {categories.map((item, index) => (
             <div>
               <Chip
-                key={name}
-                label={name}
-                variant={new Set(selected).has(name) ? "filled" : "outlined"}
-                onClick={() => handleSelect(name)}
+                key = {index}
+                label = {item.name}
+                variant={new Set(selected).has(item.name) ? "filled" : "outlined"}
+                onClick={() => handleSelect(item.name)}
               />
             </div>
           ))}
+
         </Stack>
       </Box>
     );
@@ -122,7 +114,7 @@ function Quiz() {
           <h3 style={{ color: "#818181" }}>
             Select 3 or more interests to get event suggestions based on what
             you like
-          </h3>
+          </h3>          
         </Stack>
 
         <Stack>
